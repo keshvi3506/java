64708a57642618806d678471c9c97c32846d67a1
keshvi3506 <24it039@charusat.edu.in>
2025-10-07 13:33:34 +0530

    ArrayTraverse.java

---
+++ ArrayTraverse.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Basic_Input_Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.next(); 
+
+        sc.nextLine(); 
+        System.out.print("Enter your full name: ");
+        String fullName = sc.nextLine(); 
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your height: ");
+        double height = sc.nextDouble();
+
+        System.out.println("\n--- Output ---");
+        System.out.println("Name: " + name);
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+        sc.close();
+    }
+}
